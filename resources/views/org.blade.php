c96c67e38df3e9b73697f4f45bc75680d5d32eac
Benjamin Chukwudi <benjaminchukwudi0@gmail.com>
2019-02-21 08:51:44 +0100

    fixed members

--- resources/views/org.blade.php
+++ resources/views/org.blade.php
@@ -1,3 +1,3 @@
-<object data="organchart.pdf" type="application/pdf" width="100%" height="100%">
-    <p>Alternative text - include a link <a href="organchart.pdf">to the PDF</a></p>
+<object data="/storage/v299pDtIrJq0Vqb5xyKrBLYq5VgUIw8TA8jEBETU.pdf" type="application/pdf" width="100%" height="100%">
+    <p>Alternative text - include a link <a href="/storage/v299pDtIrJq0Vqb5xyKrBLYq5VgUIw8TA8jEBETU.pdf">to the PDF</a></p>
 </object>
